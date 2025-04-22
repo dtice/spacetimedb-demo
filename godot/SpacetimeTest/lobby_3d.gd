af0efff0d9e0f3fd868125e4b676ecd6664cca3c
dill <dillon.tice@gmail.com>
2025-04-21 20:00:08 -0700

    init: spacetime driven physics

--- godot/SpacetimeTest/lobby_3d.gd
+++ godot/SpacetimeTest/lobby_3d.gd
@@ -42,6 +42,14 @@ func _on_spacetimedb_row_updated(table_name: String, row: Resource):
 	if table_name == 'user':
 		#_update_player(row) # Your function to update position, state, etc.
 		print("Updating player", row)
+		#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		#if direction:
+			#velocity.x = direction.x * SPEED
+			#velocity.z = direction.z * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+			#velocity.z = move_toward(velocity.z, 0, SPEED)
 		
 	if table_name == 'message':
 		print("Message updated", row)
